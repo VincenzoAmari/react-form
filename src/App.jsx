89a83f5dee35938b426a7ca83ecd4a0775ff9baf
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-24 16:17:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { useState } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function App() {
+  const form = useState(null);
+
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>form</h1>
+    </header>
+  );
+}
+
+export default Header;
