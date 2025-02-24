fee195668e45024f151496d37ed3390f23cd1f31
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-24 16:53:45 +0100

    posso aggiungere i campi

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,34 @@ function App() {
   const [ToDo, setTODO] = useState(ToDoList);
   const [newDo, setNewDo] = useState('');
 
+  const addDO = (e) => {
+    e.preventDefault();
+    const task = newDo.trim();
+    setTODO([...ToDo, task]);
+    setNewDo('');
+  };
+
+  
   return (
     <>
-      <form onSubmit={ToDo}>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Cose da fare oggi!!"
-            value={newDo}
-            onChange={(e) => setNewDo(e.target.value)}
-          />
+      <h1>Lista delle cose da fare</h1>
+      <ul>
+        {ToDo.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
 
-          <button className="">Aggiungi</button>
-        </form>
-      
+      <form onSubmit={addDO}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Cose da fare oggi!!"
+          value={newDo}
+          onChange={(e) => setNewDo(e.target.value)}
+        />
+        <button className="btn btn-primary mt-2">Aggiungi</button>
+      </form>
     </>
   );
 }
-
 export default App;
